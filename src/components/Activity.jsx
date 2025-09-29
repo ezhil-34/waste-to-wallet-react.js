43b5bf98dbaac19f79421466a0695ad9e5b2d38d
mayil <ezhilmayilvaganan393@gmail.com>
2025-09-29 22:02:09 +0530

    commit

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export function Activity({ title, children }) {
   return (
-    <div className="rounded-xl ml-12 p-6 mt-16 shadow-lg border border-gray-200">
+    <div className="rounded-xl ml-12 p-6 mt-16 shadow-2xl border border-gray-200">
       <h4 className="font-semibold text-brown-700 mb-2 mt-4 text-5xl">{title}</h4>
       {children}
     </div>
--- src/components/Pickup.jsx
+++ src/components/Pickup.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { MapPin } from "lucide-react";
 
 export default function Pickup( { req, onAccept }) {
 
     return(
         <div className="rounded-xl ml-12 mt-8 bg-white  p-4 flex justify-between items-center border border-white-200 shadow-lg">
-            <div>
+            <div className="ml-4">
                 <p className="text-4xl font-bold mt-4 text-gray-800">
                     {req.type}{" "}
                     <span className="ml-2 mt-4 text-xl px-2 py-1 rounded-full bg-yellow-200 text-yellow-800">
@@ -13,9 +14,12 @@ export default function Pickup( { req, onAccept }) {
                 </p>
                 <p className="text-gray-600  text-2xl mt-4">{req.details}</p>
                 <p className="text-gray-500 text-2xl  mt-4">Requested by {req.user} • {req.time} </p>
+                <p className="text-gray-500 flex items-center gap-2 text-2xl mt-4">
+                    <MapPin className="text-gray-500"/>
+                    {req.address}</p>
             </div>
             <button onClick={onAccept}
-                className="text-white bg-blue-500 px-4 py-1 rounded-full hover:bg-green-700">Accept</button>
+                className="text-white bg-blue-500 px-8 font-bold py-4 rounded-full hover:bg-green-700">Accept</button>
         </div>
     );
 }
